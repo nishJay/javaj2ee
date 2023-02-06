3cf82d40b24b69199f9db2ffe7533d1542180c39
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2023-02-06 10:45:55 +0000

    fjhdsh

--- arraylist.java
+++ arraylist.java
@@ -1,26 +1,48 @@
+package arraylist;
+
 import java.util.*;
 
 public class arraylist {
 	
+	String a,b,c;
+	
+	public arraylist(String a, String b, String c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return a+b+c;
+	}
+
 	public static void main(String args[])
 	{
-		ArrayList<String> colors = new ArrayList<>();
+		arraylist o1 = new arraylist("Maroon","Green","Grey");
 		
+		ArrayList<String> colors = new ArrayList<>();
+		colors.add(o1.a);
+		colors.add(o1.b);
+		colors.add(o1.c);
 		colors.add("Blue");
 		colors.add("Red");
 		colors.add("Orange");
 		colors.add("Yellow");
 		
+		System.out.println(o1.toString());
 		System.out.println(colors);
 		
 		for(String s:colors){
 			System.out.println(s);
-		}
-
-        for (Iterator col = colors.iterator(); col.hasNext();) {
+			
+		for (Iterator col = colors.iterator(); col.hasNext();) {
 			String s1 = (String) col.next();
 			System.out.println(s1);
 			
+		}
 		}
 	}
 
