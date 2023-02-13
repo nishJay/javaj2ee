27c3fd33c63db05d09065fc7b7c14510a621f46d
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2023-02-13 10:41:21 +0000

    tjehft

--- hashmap/src/hashmap.java
+++ hashmap/src/hashmap.java
@@ -1,22 +1,25 @@
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
-public class hashmap
-{
-    public static void main(String[] args) 
-    {
-        HashMap<Integer,String> hash = new HashMap<Integer, String>();
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hash = new HashMap<Integer, String>();
 
-        hash.put(2,"JJ");
-        hash.put(3,"JK");
-        hash.put(4,"JL");
-        hash.put(5,"JM");
+        hash.put(2, "JJ");
+        hash.put(3, "JK");
+        hash.put(4, "JL");
+        hash.put(5, "JM");
 
         System.out.println(hash);
 
+        Set<Entry<Integer, String>> set = hash.entrySet();
 
+        for (Entry<Integer, String> me : set) {
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
 
-        
     }
-    
+
 }
--- student/student.java
+++ student/student.java
@@ -2,90 +2,75 @@ package student;
 
 import java.util.*;
 
-public class student 
-{
+public class student {
 	String name;
-    String usn;
-    int age;
-    float cgpa;
+	String usn;
+	int age;
+	float cgpa;
 
-    
-
-   public student(String name, String usn, int age, float cgpa) {
-       this.name = name;
-       this.usn = usn;
-       this.age = age;
-       this.cgpa = cgpa;
-   }
-
-
-
-
-
-   public static void main(String args[]) 
-   {
-	   Scanner sc = new Scanner(System.in);
-       ArrayList<student> s = new ArrayList<student>();
-       int i;
-       
-       for(i=0;i<5;i++)
-       {
-    	   student st = new student("","",0,0);
-    	   System.out.println("Enter the student name");
-    	   st.name = sc.next();
-    	   System.out.println("Enter the student usn");
-    	   st.usn = sc.next();
-    	   System.out.println("Enter the student age");
-    	   st.age = sc.nextInt();
-    	   System.out.println("Enter the student cgpa");
-    	   st.cgpa = sc.nextFloat();    
-    	   s.add(st);
-    	   
-       }
-       
-       for (Iterator j = s.iterator(); j.hasNext();)
-       {
-    	   student sto = (student)j.next();
-    	   if(sto.cgpa > 8.0)
-    	   {
-    		   System.out.println(sto.name);
-    	   }
-    	   
-		
-	}
-       
-       for (Iterator j = s.iterator(); j.hasNext();)
-       {
-    	   student sto = (student)j.next();
-    	   if(sto.name.startsWith("a"))
-    	   {
-    		   System.out.println(sto.name);
-    	   }
-    	   
-		
+	public student(String name, String usn, int age, float cgpa) {
+		this.name = name;
+		this.usn = usn;
+		this.age = age;
+		this.cgpa = cgpa;
 	}
 
-	for (Iterator j = s.iterator(); j.hasNext();)
-       {
-    	   student sto = (student)j.next(); 
-		   System.out.println(s.indexOf(sto)+": "+ sto.name);    
-    	  	   
-    	}
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<student> s = new ArrayList<student>();
+		int i;
 
-	System.out.println("Enter the index of the student whose name has to be updated");
-	Integer usnch = sc.nextInt();
-	System.out.println("Enter the new name has to be updated");
-	String naam = sc.next();
+		for (i = 0; i < 5; i++) {
 
-	for (Iterator j = s.iterator(); j.hasNext();)
-       {
-    	   student sto = (student)j.next(); 
-		   if(s.indexOf(sto)==usnch){
-			sto.name = naam;			
-		   }
-		   System.out.println(s.indexOf(sto)+": "+ sto.name);    
-    	  	   
-    	}       
-   }
+			System.out.println("Enter the details for student" + i);
+			student st = new student("", "", 0, 0);
+			System.out.println("Enter the student name");
+			st.name = sc.next();
+			System.out.println("Enter the student usn");
+			st.usn = sc.next();
+			System.out.println("Enter the student age");
+			st.age = sc.nextInt();
+			System.out.println("Enter the student cgpa");
+			st.cgpa = sc.nextFloat();
+			s.add(st);
+
+		}
+		System.out.println("The students with cgpa > 8.0");
+		for (Iterator j = s.iterator(); j.hasNext();) {
+			student sto = (student) j.next();
+			if (sto.cgpa > 8.0) {
+				System.out.println(sto.name);
+			}
+
+		}
+		System.out.println("The students with name starting with a");
+		for (Iterator j = s.iterator(); j.hasNext();) {
+			student sto = (student) j.next();
+			if (sto.name.startsWith("a")) {
+				System.out.println(sto.name);
+			}
+
+		}
+System.out.println("The names with indices");
+		for (Iterator j = s.iterator(); j.hasNext();) {
+			student sto = (student) j.next();
+			System.out.println(s.indexOf(sto) + ": " + sto.name);
+
+		}
+
+		System.out.println("Enter the index of the student whose name has to be updated");
+		Integer usnch = sc.nextInt();
+		System.out.println("Enter the new name has to be updated");
+		String naam = sc.next();
+
+		for (Iterator j = s.iterator(); j.hasNext();) {
+			student sto = (student) j.next();
+			if (s.indexOf(sto) == usnch) {
+				sto.name = naam;
+			}
+			System.out.println(s.indexOf(sto) + ": " + sto.name);
+
+		}
+	}
 
 }
