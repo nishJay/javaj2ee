fe7ee79d8707251c613c1158bf98a7246b6e9883
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2023-02-20 11:00:07 +0000

    mm

---
+++ call/main.java
@@ -0,0 +1,24 @@
+package call;
+
+import java.util.*;
+
+public class main {
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        LinkedList<missedCallDetails> ll = new LinkedList<>();
+
+        System.out.println("Enter the choice");
+        System.out.println("1: To print the contact list\n
+                            2: To ");
+
+        while(true)
+        {
+            switch ch:
+
+        }
+    }
+    
+}
---
+++ call/missedCallDetails.java
@@ -0,0 +1,23 @@
+package call;
+
+import java.util.*;
+
+public class missedCallDetails {
+
+    Calendar cal;
+    String ph;
+    String name;
+
+    public missedCallDetails(Calendar cal, String ph, String name) {
+        this.cal = cal;
+        this.ph = ph;
+        this.name = name;
+    }
+
+    
+
+    
+
+
+    
+}
