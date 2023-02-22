3f11eb1271e288d2c7a973d39310af2263dca4df
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2023-02-22 10:48:34 +0530

    Create Books.java

---
+++ q2/Books.java
@@ -0,0 +1,23 @@
+package Question2;
+
+public class Books 
+{
+	String title;
+	String author;
+	String publisher;
+	float price;
+	
+	
+	public Books(String title, String author, String publisher, float price) {
+		
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.price = price;
+	}
+	
+	
+	
+	
+
+}
